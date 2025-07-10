ec995fa857017091c8208c29eb6a9eac844fef7c
Delta Student <tudulaxmi050@gmail.com>
2025-07-10 10:01:08 +0530

    updated files

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,23 @@ import morgan from "morgan";
 import transactionRoutes from "./Routers/Transactions.js";
 import userRoutes from "./Routers/userRouter.js";
 import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config({ path: "./config/config.env" });
+
 const app = express();
+const port = process.env.PORT || 8080;
 
-const port = process.env.PORT;
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 connectDB();
 app.use(express.json());
-const allowedOrigins = ["http://localhost:3000"];
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://moneytrack23.netlify.app", 
+];
+
 app.use(
   cors({
     origin: allowedOrigins,
@@ -30,12 +38,17 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
 app.use("/api/v1", transactionRoutes);
 app.use("/api/auth", userRoutes);
-app.post("/login", async (req, res) => {
-  console.log("Login route hit:", req.body);
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
+// Start server
 app.listen(port, () => {
-  console.log(`Server is listening on http://localhost:${port}`);
+  console.log(` Server is listening on http://localhost:${port}`);
 });
